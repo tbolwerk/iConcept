1116fd03bb092f587e11ee6d4d6f5ba276efe682
suzanneb <38525856+suzannebogaard@users.noreply.github.com>
2018-04-26 14:58:17 +0200

    edit
    add commentas

--- logout.php
+++ logout.php
@@ -5,8 +5,11 @@
  * Date: 25-12-2017
  * Time: 15:10
  */
+ //session: all data is retrieved
 session_start();
+//all retrieved data gets deleted
 session_destroy();
+//after the delete, the website redirects to the index
 header("Location:index.php");
 
 ?>

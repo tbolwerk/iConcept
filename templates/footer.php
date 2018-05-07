040e8ee298963bdb2de9f1a83897f9c7fdec30ab
Mkalil <Michaelie_X@hotmail.com>
2018-05-07 15:17:36 +0200

    Add
    Added overlay script

--- templates/footer.php
+++ templates/footer.php
@@ -12,6 +12,10 @@
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Classie.js -->
+    <!-- <script type="text/javascript" src="js/classie.js"></script> -->
+    <!-- Rubrieken overlay JavaScript -->
+    <script type="text/javascript" src="js/category_overlay.js"></script>
 </body>
 
 </html>

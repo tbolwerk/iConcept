3cd2763f924cd74c2a884920a5458a82be0a692b
aukeon <38281262+aukeon@users.noreply.github.com>
2018-05-08 15:23:48 +0200

    regsitratie en verificatie als verkoper aangemaakt

--- register_seller.php
+++ register_seller.php
@@ -1,38 +1,100 @@
-<form action="" method="post" enctype="multipart/form-data">
-  <label>Titel</label>
-  <input type="text" name="title" value="<?php if(isset($_POST['title'])){echo $_POST['title'];}?>"><br>
-  <?php if(isset($errors['title'])){echo $errors['title'];}?><br>
-  <label>Beschrijving</label>
-  <textarea name="description" value=""><?php if(isset($_POST['description'])){echo $_POST['description'];}?></textarea><br>
-  <?php if(isset($errors['description'])){echo $errors['description'];}?><br>
-  <label>Startprijs</label>
-  <input type="text" name="startprice" value="<?php if(isset($_POST['startprice'])){echo $_POST['startprice'];}?>"><br>
-  <label>Looptijd</label>
-  <select name="duration">
-    <option value="1">1 dagen</option>
-    <option value="3">3 dagen</option>
-    <option value="5">5 dagen</option>
-    <option value="7" selected>7 dagen</option>
-    <option value="10">10 dagen</option>
+<?php
+$current_page='register_seller';
+require_once('templates/header.php');
+require_once('templates/connect.php');
+$username = $_SESSION['username'];
+
+
+function createVerificationCodeSeller($username, $random_password) {
+	global $dbh;
+	global $error;
+
+    try {
+		$userdata = $dbh->prepare("insert into VerificatieVerkoper(gebruikersnaam, code) Values(?, ?, ?)");
+		$userdata->execute(array($username, $random_password));
+    } catch (PDOException $e) {
+		$error=$e;
+    }
+}
+
+function registerSeller($username, $checkoption, $creditcard, $bank, $banknumber){
+  global $dbh;
+  global $errors;
+  $errors = array();
+
+  try {
+    $data = $dbh->prepare("select verkoper from Gebruiker where gebruikersnaam = ?");
+    $data->execute(array($username));
+    $seller = $data->fetch();
+
+  } catch (PDOException $e) {
+    $error=$e;
+    echo $error;
+  }
+  if($seller[0] == 1)//checks if user is seller
+  {
+    $errors['seller'] = "U bent al een verkoper.";
+  }
+  else {
+    if(empty($checkoption))//checks if checkoption is not empty
+    {
+      $errors['checkoption'] = "Dit is een verplicht veld.";
+    }
+    if($checkoption == "creditcard" && empty($creditcard))//checks if title is not empty and if creditcardoption is selected
+    {
+      $errors['creditcard'] = "Dit is een verplicht veld.";
+    }
+    if(empty($bank) && empty($creditcard) && $checkoption != "creditcard"){
+      $errors['bank'] = "Dit is een verplicht veld.";
+    }
+    if(empty($banknumber) && empty($creditcard) && $checkoption != "creditcard"){
+      $errors['banknumber'] = "Dit is een verplicht veld.";
+    }
+
+    if(count($errors) == 0){//checks if there are errors
+      try {
+        $data = $dbh->prepare("insert into Verkoper(gebruikersnaam, controleoptienaam, creditcardnummer, banknaam, rekeningnummer) values(?, ?, ?, ?, ?)");
+        $data->execute(array($username, $checkoption, $creditcard, $bank, $banknumber));
+      }
+      catch (PDOException $e) {
+        $error=$e;
+        echo $error;
+      }
+      if($checkoption == "creditcard"){
+        $userdata = $dbh->prepare("update Gebruiker set verkoper = 1 where gebruikersnaam = ?;");
+        $userdata->execute(array($username));
+      }
+      else {
+        $code = random_password(6);
+  			createVerificationCode($username, $code);
+      }
+    }
+  }
+
+}
+
+if(isset($_POST['submit'])){
+  registerSeller($username, $_POST['checkoption'],$_POST['creditcard'],$_POST['bank'],$_POST['banknumber']);
+}
+
+?>
+
+
+
+<form action="" method="post">
+  <label>Controleoptie</label>
+  <select name="checkoption">
+    <option value="post">Code via post</option>
+    <option value="creditcard">Creditcard</option>
   </select><br>
-  <label>Betalingswijze</label>
-  <input type="radio" name="pay_method" value="contant" checked> Contant
-  <input type="radio" name="pay_method" value="bank/giro"> Bank/Giro
-  <input type="radio" name="pay_method" value="anders"> Anders<br>
-  <label>Betalingsinstructies</label>
-  <input type="text" name="pay_instructions"><br>
-  <label>Plaats</label>
-  <input type="text" name="place"><br>
-  <label>Land</label>
-  <input type="text" name="country"><br>
-  <label>Verzendkosten</label>
-  <input type="text" name="shipping_costs"><br>
-  <label>Verzendinstructies</label>
-  <input type="text" name="shipping_instructions"><br>
-  <label>Afbeeldingen</label><br>
-  <input name="picture[]" type="file"><br>
-	<input name="picture[]" type="file"><br>
-	<input name="picture[]" type="file"><br>
-	<input name="picture[]" type="file"><br>
-  <button type="submit" name="submit">Plaats veiling</button>
+  <label>Creditcardnummer</label>
+  <input type="text" name="creditcard" value="<?php if(isset($_POST['creditcard'])){echo $_POST['creditcard'];}?>"><br>
+  <?php if(isset($errors['creditcard'])){echo $errors['creditcard'];}?><br>
+  <label>Banknaam</label>
+  <input type="text" name="bank" value="<?php if(isset($_POST['bank'])){echo $_POST['bank'];}?>"><br>
+  <?php if(isset($errors['bank'])){echo $errors['bank'];}?><br>
+  <label>Rekeningnummer</label>
+  <input type="text" name="banknumber" value="<?php if(isset($_POST['banknumber'])){echo $_POST['banknumber'];}?>"><br>
+  <?php if(isset($errors['banknumber'])){echo $errors['banknumber'];}?><br>
+  <button type="submit" name="submit">Word verkoper</button>
 </form>
---
+++ verification_seller.php
@@ -0,0 +1,38 @@
+<?php
+$current_page='register_verification';
+require_once('templates/header.php');
+require_once('templates/connect.php');
+$username = $_SESSION['username'];
+
+function verificationSeller($username,$code)
+{
+	$codeValid = true;//codeValid is true until proven that it's not
+
+	try {//checks if code exists in database
+	$statement = $dbh->prepare("select * from VerificatieVerkoper where gebruikersnaam = ? AND code = ?");
+	$statement->execute(array($username,$code));
+	$results = $statement->fetch();
+	} catch (PDOException $e) {
+	$error= "Code invalid";
+	$codeValid = false;
+	}
+
+	if ($codeValid) {
+	$statement = $dbh->prepare("update Gebruiker set verkoper = 1 where gebruikersnaam = ?");
+	$statement->execute(array($username));
+	$statement = $dbh->prepare("delete VerificatieVerkoper where gebruikersnaam = ?");
+	$statement->execute(array($username));
+	}
+}
+
+if(isset($_POST['submit'])){
+  verificationSeller($username, $_POST['code']);
+}
+
+?>
+
+<form action="" method="post">
+  <label>Verificatiecode</label>
+	<input type="text" name="code"><br>
+	<button type="submit" name="submit">Verifieer</button>
+</form>

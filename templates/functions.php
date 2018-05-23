c9f0e9e255e1aa23b3f80fc3062b9e83d55f4414
janbeenham <twanbolwerk@gmail.com>
2018-05-23 18:55:45 +0200

    rubrieken + backend

--- templates/functions.php
+++ templates/functions.php
@@ -167,6 +167,61 @@ function displayAuction()
 
 }
 
+ /*display auctionpage*/
+ function displayAuctionpage()
+ {
+ 
+ 
+   global $dbh;
+   global $auctionpage;
+   $auction = "<script>
+         var countDownDate = [];
+         </script>";
+ 
+   try{
+     $data = $dbh->query("SELECT * FROM Voorwerp vw INNER JOIN Bestand b ON vw.voorwerpnummer=b.voorwerpnummer");
+ 
+     while ($row = $data->fetch()) {
+  
+       $looptijd = $row['looptijd'];
+       $looptijdbegindag =strtotime($row['looptijdbegindag']);
+       $looptijdbegintijdstip = strtotime($row['looptijdtijdstip']);
+       $countdown_date = date("Y-m-d",$looptijdbegindag);
+       $countdown_time = date("h:i:s",$looptijdbegintijdstip);
+       $countdown = $countdown_date . " " . $countdown_time;
+ 
+ 
+ 
+       $auctionpage.='  <div class="card auction-card mb-4 col-md-4">
+       <div class="view overlay">
+         <img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
+       </div>
+       <div class="card-body">
+         <span class="small-font">20345322</span>
+         <h4 class="card-title">'.$row["titel"].'</h4>
+         <hr>
+         <div class="card-text">
+           <p>
+             '.$row["beschrijving"].'
+           </p>
+         </div>
+         <hr />
+         <ul class="list-unstyled list-inline d-flex" style="text-align:center">
+           <li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
+           <div class="card-line"></div>
+           <li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
+         </ul>
+       </div>
+     </div>
+ 
+           ';
+     }
+   }catch(PDOException $e){
+     $error = $e;
+   }
+ 
+ }
+
 /*verification function*/
 function verification($getUsername,$getCode)
 {

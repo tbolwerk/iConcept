f74d9d090f59415fd06c123bca1e779a25095449
Twan Bolwerk <twanbolwerk@gmail.com>
2018-05-23 14:20:09 +0200

    modify caruousel with php code

--- templates/carousel.php
+++ templates/carousel.php
@@ -1,53 +1,34 @@
 <?php require_once("functions.php");
-function displayCarousel()
-{
-
-
-	global $dbh;
-	global $carousel;
-	$carousel = "";
-
-	try{
-		$data = $dbh->query("select * from Voorwerp");
-    $counter = 0;
-		while ($row = $data->fetch()) {
-
-
-			$carousel.='          <!--First slide-->
-              <div class="carousel-item active">
-
-                <div class="col-md-3">
-                    <div class="card auction-card mb-4">
-                      <div class="view overlay">
-                        <img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-                      </div>
-                      <div class="card-body">
-                        <span class="small-font">20345322</span>
-                        <h4 class="card-title">'.$row["titel"].'</h4>
-                        <hr>
-                        <div class="card-text">
-                          <p>
-                        '.$row["beschrijving"].'
-                          </p>
-                        </div>
-                        <hr />
-                        <ul class="list-unstyled list-inline d-flex" style="text-align:center">
-                          <li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-                          <div class="card-line"></div>
-                          <li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-                        </ul>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>';
-		}
-	}catch(PDOException $e){
-		$error = $e;
-	}
-
+global $dbh;
+$statement = $dbh->query("SELECT TOP 4 * FROM Voorwerp vw INNER JOIN Bestand b ON vw.voorwerpnummer=b.voorwerpnummer");
+$carousel="";
+while($row = $statement->fetch()){
+$carousel.=	'			<div class="col-md-3">
+					<div class="card auction-card mb-4">
+						<div class="view overlay">
+							<img class="card-img-top" src="'.$row["filenaam"].'" alt="'.$row["titel"].'" />
+						</div>
+						<div class="card-body">
+							<span class="small-font">'.$row["voorwerpnummer"].'</span>
+							<h4 class="card-title">'.$row["titel"].'</h4>
+							<hr>
+							<div class="card-text">
+								<p>
+									'.$row["beschrijving"].'
+								</p>
+							</div>
+							<hr />
+							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
+								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;'.$row["startprijs"].'</li>
+								<div class="card-line"></div>
+								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
+							</ul>
+						</div>
+					</div>
+				</div>';
 }
-displayCarousel();
+
+
 ?>
 <!--Carousel Wrapper-->
 <div id="multi-item-example" class="carousel slide carousel-multi-item mt-5" data-ride="carousel">
@@ -55,294 +36,14 @@ displayCarousel();
     <!--Slides-->
     <div class="carousel-inner" role="listbox">
 			<div class="carousel-item active">
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
+<?=$carousel?>
 			<!-- Second slide -->
 			<div class="carousel-item">
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="card auction-card mb-4">
-						<div class="view overlay">
-							<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-						</div>
-						<div class="card-body">
-							<span class="small-font">20345322</span>
-							<h4 class="card-title">'.$row["titel"].'</h4>
-							<hr>
-							<div class="card-text">
-								<p>
-									'.$row["beschrijving"].'
-								</p>
-							</div>
-							<hr />
-							<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-								<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-								<div class="card-line"></div>
-								<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-							</ul>
-						</div>
-					</div>
-				</div>
+<?=$carousel?>
 			</div>
 				<!-- Third slide -->
 				<div class="carousel-item">
-					<div class="col-md-3">
-						<div class="card auction-card mb-4">
-							<div class="view overlay">
-								<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-							</div>
-							<div class="card-body">
-								<span class="small-font">20345322</span>
-								<h4 class="card-title">'.$row["titel"].'</h4>
-								<hr>
-								<div class="card-text">
-									<p>
-										'.$row["beschrijving"].'
-									</p>
-								</div>
-								<hr />
-								<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-									<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-									<div class="card-line"></div>
-									<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-md-3">
-						<div class="card auction-card mb-4">
-							<div class="view overlay">
-								<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-							</div>
-							<div class="card-body">
-								<span class="small-font">20345322</span>
-								<h4 class="card-title">'.$row["titel"].'</h4>
-								<hr>
-								<div class="card-text">
-									<p>
-										'.$row["beschrijving"].'
-									</p>
-								</div>
-								<hr />
-								<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-									<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-									<div class="card-line"></div>
-									<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-md-3">
-						<div class="card auction-card mb-4">
-							<div class="view overlay">
-								<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-							</div>
-							<div class="card-body">
-								<span class="small-font">20345322</span>
-								<h4 class="card-title">'.$row["titel"].'</h4>
-								<hr>
-								<div class="card-text">
-									<p>
-										'.$row["beschrijving"].'
-									</p>
-								</div>
-								<hr />
-								<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-									<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-									<div class="card-line"></div>
-									<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="card auction-card mb-4">
-							<div class="view overlay">
-								<img class="card-img-top" src="https://picsum.photos/388/258/?image=4" alt="Test Card" />
-							</div>
-							<div class="card-body">
-								<span class="small-font">20345322</span>
-								<h4 class="card-title">'.$row["titel"].'</h4>
-								<hr>
-								<div class="card-text">
-									<p>
-										'.$row["beschrijving"].'
-									</p>
-								</div>
-								<hr />
-								<ul class="list-unstyled list-inline d-flex" style="text-align:center">
-									<li class="list-inline-item pr-2 flex-1 ml-5"><i class="fa fa-lg fa-gavel pr-2"></i>&euro;40,50</li>
-									<div class="card-line"></div>
-									<li class="list-inline-item pr-2 flex-1 mr-5"><i class="fa fa-lg fa-clock pr-2"></i>02:01:21</li>
-								</ul>
-							</div>
-						</div>
-					</div>
+<?=$carousel?>
 				</div>
 
   <!--/.Third slide-->

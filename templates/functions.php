8f0514e12838fa128efc61bc824922ed8b7bc0f5
Twan Bolwerk <twanbolwerk@gmail.com>
2018-05-24 10:17:28 +0200

    fix nav

--- templates/functions.php
+++ templates/functions.php
@@ -87,10 +87,10 @@ function displayColumn(){
     $data = $dbh->query("SELECT * FROM Rubriek");
     while($row = $data->fetch()){
 			if($row['rubrieknummerOuder'] == -1){//als rubrieknummerOuder == -1 geeft hoofdrubrieken
-      $column.="<a href='?rubrieknummer=".$row['rubrieknummer']."'>".$row['rubrieknaam']."</a>";
+      $column.="<a href='rubriek.php?rubrieknummer=".$row['rubrieknummer']."'>".$row['rubrieknaam']."</a>";
 		}else if(isset($_GET['rubrieknummer'])){//als er een get request is , zoekt bij behorende rubrieknummerOuder
 			if($row['rubrieknummerOuder'] == $_GET['rubrieknummer']){
-				$column.="<a href='?rubrieknummer=".$row['rubrieknummer']."'>".$row['rubrieknaam']."</a>";
+				$column.="<a href='rubriek.php?rubrieknummer=".$row['rubrieknummer']."'>".$row['rubrieknaam']."</a>";
 			}
 		}
     }

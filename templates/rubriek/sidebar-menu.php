09ffc5c876a072291e5b8c03781343002ba8880a
Twan Bolwerk <twanbolwerk@gmail.com>
2018-05-25 21:37:02 +0200

    modify sidebar without styling

--- templates/rubriek/sidebar-menu.php
+++ templates/rubriek/sidebar-menu.php
@@ -1,101 +1,90 @@
-<div class="offcanvas flypanels-left">
-  <div class="category-title">
-    <h3>Rubrieken</h3>
-  </div>
-  <div class="panelcontent" data-panel="treemenu">
-    <nav class="flypanels-treemenu" role="navigation">
-      <ul>
-        <li class="haschildren"><div class="link"><a href="#" class="link">Sub-rubriek 1</a> <a href="#" class="expand">2<i class="fa icon"></i></a></div>
-          <ul>
-            <li><div><a href="#" class="link">Sub-sub-rubriek</a></div></li>
-            <li><div><a href="#" class="link">Sub-sub-rubriek</a></div></li>
-          </ul>
-        </li>
-        <li class="haschildren"><div class="link"><a href="#" class="link">Sub-rubriek 2</a> <a href="#" class="expand">5<i class="fa icon"></i></a></div>
-          <ul>
-            <li class="haschildren"><div class="link"><a href="#" class="link">Sub-rubriek 2.1</a> <a href="#" class="expand">6<i class="fa icon"></i></a></div>
-              <ul>
-                <li class="active"><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-              </ul>
-            </li>
-            <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">11<i class="fa icon"></i></a></div>
-              <ul>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">8<i class="fa icon"></i></a></div>
-                  <ul>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                  </ul>
-                </li>
-                <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">11<i class="fa icon"></i></a></div>
-                  <ul>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                    <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">11<i class="fa icon"></i></a></div>
-                      <ul>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                      </ul>
-                    </li>
-                    <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">11<i class="fa icon"></i></a></div>
-                      <ul>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                        <li><div><a href="#" class="link">Example menu item</a></div></li>
-                      </ul>
-                    </li>
-                    <li><div><a href="#" class="link">Example menu item</a></div></li>
-                  </ul>
-                </li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-              </ul>
-            </li>
-            <li><div><a href="#" class="link">Example menu item</a></div></li>
-            <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">5<i class="fa icon"></i></a></div>
-            <ul>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-                <li><div><a href="#" class="link">Example menu item</a></div></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">1<i class="fa icon"></i></a></div>
-          <ul>
-            <li><div><a href="#" class="link">Example menu item</a></div></li>
-          </ul>
-        </li>
-        <li><div><a href="#" class="link">Example menu item</a></div></li>
-        <li><div><a href="#" class="link">Example menu item</a></div></li>
-        <li class="haschildren"><div class="link"><a href="#" class="link">Example menu item</a> <a href="#" class="expand">4<i class="fa icon"></i></a></div>
-          <ul>
-            <li><div><a href="#" class="link">Example menu item</a></div></li>
-            <li><div><a href="#" class="link">Example menu item</a></div></li>
-            <li><div><a href="#" class="link">Example menu item</a></div></li>
-            <li><div><a href="#" class="link">Example menu item</a></div></li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </div>
-</div>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/iConcept/templates/functions.php';
+
+$statement = $dbh->query("SELECT * FROM Rubriek");
+while($row = $statement->fetch()){
+  $rows[] = (array('id' => $row['rubrieknummer'],'parent_id' => $row['rubrieknummerOuder'],'name' => $row['rubrieknaam']));
+}    
+
+
+
+
+              // covert raw result set to tree
+              $menu = convertAdjacencyListToTree(null,$rows,'id','parent_id','links');
+              // echo '<pre>',print_r($menu),'</pre>';
+
+              // display menu
+              echo themeMenu($menu,1);
+
+              /*
+              * ------------------------------------------------------------------------------------
+              * Utility functions
+              * ------------------------------------------------------------------------------------
+              */
+
+              /*
+              * Convert adjacency list to hierarchical tree
+              *
+              * @param value of root level parent most likely null or 0
+              * @param array result
+              * @param str name of primary key column
+              * @param str name of parent_id column - most likely parent_id
+              * @param str name of index that children will reside ie. children, etc
+              * @return array tree
+              */
+              function convertAdjacencyListToTree($intParentId,&$arrRows,$strIdField,$strParentsIdField,$strNameResolution) {
+
+                  $arrChildren = array();
+
+                  for($i=0;$i<count($arrRows);$i++) {
+                      if($intParentId === $arrRows[$i][$strParentsIdField]) {
+                          $arrChildren = array_merge($arrChildren,array_splice($arrRows,$i--,1));
+                      }
+                  }
+
+                  $intChildren = count($arrChildren);
+                  if($intChildren != 0) {
+                      for($i=0;$i<$intChildren;$i++) {
+                          $arrChildren[$i][$strNameResolution] = convertAdjacencyListToTree($arrChildren[$i][$strIdField],$arrRows,$strIdField,$strParentsIdField,$strNameResolution);
+                      }
+                  }
+
+                  return $arrChildren;
+
+              }
+
+              /*
+              * Theme menu
+              *
+              * @param array menu
+              * @param runner (depth)
+              * @return str themed menu
+              */
+              function themeMenu($menu,$runner) {
+
+                  $out = '';
+
+                  if(empty($menu)) {
+                      return $out;
+                  }
+
+                  $out.='<ul>';
+                  foreach($menu as $link) {
+                      $out.= sprintf(
+                          '<li class="depth-%u">%s%s</li>'
+                          ,$runner
+                          ,$link['name']
+                          ,themeMenu($link['links'],($runner+1))
+                      );
+                  }
+
+                  $out.='</ul>';
+                  return $out;
+
+              }
+
+              echo 'this is working';
+              print_r($rows);
+
+
+?>

e9680d0b571df7942d211127e28868c6f38a455d
JannoOnink <38607600+JannoOnink@users.noreply.github.com>
2018-05-28 16:04:57 +0200

    modify detailpage

--- detailpage.php
+++ detailpage.php
@@ -2,16 +2,23 @@
 $current_page='detailpage';
 require_once('templates/header.php');
 
-if (isset($_GET['id'])) {
+if (isset($_GET['id'])) { //Dit hele ding is nog een WIP
   $error = "";
   if (isset($_POST['bid'])) {
-    try {
-      $bid = str_replace("\"", "", strip_tags($_POST['bid']));
-
-      $statement = $dbh->prepare("insert into bod(voorwerpnummer, bodbedrag, gebruikersnaam, boddag, bodtijdstip) Values (?, ?, ?, GETDATE(), CURRENT_TIMESTAMP)");
-      $statement->execute(array($_GET['id'], $bid, $_SESSION['username']));
-    } catch(PDOException $e){
-      $error = "Placeholder " . $e;
+    $bid = str_replace("\"", "", strip_tags($_POST['bid']));
+    if ($bid <= 0) {
+      $error = "Bod is te laag";
+    }
+    if ($error == "") {
+      try {
+        $statement = $dbh->prepare("insert into bod(voorwerpnummer, bodbedrag, gebruikersnaam, boddag, bodtijdstip) Values (?, ?, ?, GETDATE(), CURRENT_TIMESTAMP)");
+        $statement->execute(array($_GET['id'], $bid, $_SESSION['username']));
+      } catch(PDOException $e){
+        $error = $e->getCode();
+        if ($error == 23000) {
+          $error = "Primary key conflict";
+        }
+      }
     }
   }
 
@@ -24,8 +31,7 @@ if (isset($_GET['id'])) {
   $biddings = $statement->fetch();
 
   $statement = $dbh->prepare("select bodbedrag, gebruikersnaam from Bod where voorwerpnummer = ? and bodbedrag = (
-  	select max(bodbedrag) from Bod where voorwerpnummer = ?
-  	)");
+  	select max(bodbedrag) from Bod where voorwerpnummer = ?)");
   $statement->execute(array($_GET['id'], $_GET['id']));
   $maxbid = $statement->fetch();
 
@@ -35,8 +41,6 @@ if (isset($_GET['id'])) {
 
   $time = date_create($results['looptijdeindedag2'] . $results['looptijdtijdstip']);
   $closingtime = date_format($time, "d M Y H:i"); //for example 14 Jul 2020 14:35
-
-  //select looptijdbegindag, looptijd, dateadd(day, looptijd, looptijdbegindag) as looptijdeindedag from Voorwerp
 
   $statement = $dbh->prepare("select * from Rubriek where rubrieknummer = ?");
   $statement->execute(array($results['rubrieknummer']));
@@ -52,32 +56,11 @@ if (isset($_GET['id'])) {
   $lijst = array_reverse($lijst);
 
   $hoofdrubriek = $lijst[0];
+
+  $minIncrease = 1;
+  $minBidAmount = $maxbid[0] + $minIncrease;
 }
 ?>
-
-<!--
-These values are for debugging purposes and are visible by inspecting the page source
-
-<p>ontvangen post gegevens: </p>
-<?php print_r($_POST); ?><br>
-<br>
-
-<p>ontvangen get gegevens: </p>
-<?php print_r($_GET); ?><br>
-<br>
-
-<p>ontvangen database gegevens: </p>
-<?php print_r($results); ?><br>
-<br>
-
-<p>ontvangen rubriek gegevens: </p>
-<?php print_r($rubriek); ?><br>
-<br>
-
-<p>ontvangen lijst gegevens: </p>
-<?php print_r($lijst); ?><br>
-<br>
--->
 
 <!-- Banner -->
 <div class="view index-header">
@@ -87,34 +70,93 @@ These values are for debugging purposes and are visible by inspecting the page s
   </div>
 </div>
 
-<p><a href="index.php">Home</a>
-<?php
-foreach ($lijst as $rubriek) {
-  echo " > <a href=\"rubriek.php?rubrieknummer={$rubriek['rubrieknummer']}\">{$rubriek['rubrieknaam']}</a>";
-}
-?>
-</p>
-
 <p>Sluit op <?=$closingtime?></p>
-
-<p>Veilingnummer <?=$results['voorwerpnummer']?></p>
 
 <p id="maxbid">Hoogste bod <?=$maxbid[0]?> door <?=$maxbid[1]?></p>
 
-<p id="timer">Dit moet nog opgelost worden</p>
+<div class="container-fluid">
+  <h5><strong>
+    <a class="black-text" href="index.php">Home</a>
+    <?php
+    foreach ($lijst as $rubriek) {
+      echo " > <a class='black-text' href=\"rubriek.php?rubrieknummer={$rubriek['rubrieknummer']}\">{$rubriek['rubrieknaam']}</a>";
+    }
+    ?>
+  </strong></h5>
 
-<p><?=$error?></p>
+  <div class="row">
+    <div class="col-md-7">
 
-<form method="post" class="col-md-6">
-    <input type="number" name="bid" id="bid" class="form-control">
-    <button type="submit" name="submit" class="btn btn-primary">Bied</button>
-</form>
+      <?php //Display images
+      foreach ($bestanden as $bestand) {
+        echo "<img src=\"{$bestand['filenaam']}\"></img>";
+      }
+      ?>
 
-<?php
-foreach ($bestanden as $bestand) {
-  echo "<img src=\"{$bestand['filenaam']}\"></img>";
-}
-?>
+      <p><?=$results['betalingswijze']?></p>
+
+    </div>
+
+    <div class="col-md-5">
+
+      <h2><strong><?=$results['titel']?></strong></h2>
+
+      <hr>
+
+      <div class="row text-center">
+        <div class="col">
+          <p><?=$maxbid[0]?> door <?=$maxbid[1]?></p>
+        </div>
+        <div class="col">
+          <p id="timer">Dit moet nog opgelost worden</p>
+        </div>
+      </div>
+
+      <hr>
+
+      <form method="post" class="row">
+          <div class="col"></div>
+          <input type="number" name="bid" class="form-control col-md-5" required min="<?=$minBidAmount?>">
+          <button type="submit" name="submit" class="btn btn-primary col-md-3">Bied</button>
+          <div class="col"></div>
+      </form>
+
+      <p class="red-text text-center"><strong><?=$error?></strong></p>
+
+      <hr>
+
+      <p>Startprijs: <?=$results['startprijs']?></p>
+      <p><?=$results['plaatsnaam']?>, <?=$results['land']?></p>
+      <p>Sluit op: <?=$closingtime?></p>
+      <p><?=$results['betalingsinstructie']?></p>
+      <p>Veilingnummer: <?=$results['voorwerpnummer']?></p>
+
+    </div>
+  </div>
+
+  <h4>Productomschrijving</h4>
+
+  <hr>
+  <!-- <p><?=$results['beschrijving']?></p> -->
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam justo, ultricies eget enim a, viverra rutrum massa.
+    Praesent semper faucibus luctus. Donec condimentum interdum augue, eget scelerisque justo tempor a. Suspendisse orci dolor, lobortis quis lacus quis,
+    aliquam mollis enim. Integer finibus venenatis arcu id viverra. Cras nec enim sed dolor laoreet efficitur quis sed arcu. Fusce ligula sapien, tincidunt
+    eget quam ac, hendrerit tristique erat. Aliquam ullamcorper nibh ac ipsum volutpat tincidunt. Etiam et felis orci.
+  </p>
+  <p>
+    Ut tellus nisl, ultricies vehicula iaculis sed, scelerisque at orci. Aliquam id vulputate ex. Nunc dolor erat, mollis eget rutrum ut, laoreet ac sapien. Sed imperdiet diam at arcu hendrerit, non convallis risus aliquet. Fusce quis lobortis purus. Maecenas feugiat rhoncus lacus sagittis ornare. Mauris ac placerat orci. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+  </p>
+  <p>
+    Integer dapibus metus risus, tempor ultricies tortor tincidunt sed. Suspendisse vitae ligula nec dolor finibus efficitur at a metus. Aenean ullamcorper urna metus. Vestibulum mollis consequat placerat. Duis imperdiet tellus imperdiet scelerisque molestie. Vestibulum eget urna id purus mollis malesuada sit amet vitae mi. Quisque non porttitor augue. Nulla felis odio, malesuada finibus tincidunt id, porttitor elementum justo. Proin eu mi eu nisl malesuada ultricies. Integer sed ligula dignissim nulla sodales egestas. Aliquam vel ligula venenatis, vulputate augue quis, dignissim quam. Proin ex diam, sodales a aliquam vel, accumsan eu lacus. Etiam posuere efficitur urna. Maecenas molestie faucibus felis, in pellentesque nisi sagittis ut. Suspendisse eget est ut purus ultricies blandit. Maecenas vel pellentesque sem.
+  </p>
+  <p>
+    Nulla aliquam ipsum odio, et egestas dolor lacinia vitae. Duis non laoreet erat, sit amet volutpat elit. Sed efficitur arcu vitae neque auctor, a scelerisque ipsum commodo. Sed cursus velit in ex facilisis lobortis. Nam pretium odio nibh, et blandit nulla vehicula a. Quisque euismod, metus eget convallis facilisis, nibh odio ultrices tellus, vel ultricies metus nulla quis massa. Nunc vulputate neque quis mauris tempus, et congue velit rhoncus. Pellentesque dignissim mollis nisl sit amet efficitur. Quisque venenatis volutpat tellus, molestie ullamcorper sapien rhoncus sit amet.
+  </p>
+  <p>
+    Pellentesque congue vehicula neque ut vestibulum. Aenean leo urna, tempor in venenatis et, aliquam elementum felis. Aliquam condimentum felis facilisis tempus commodo. Duis semper mi vel nulla tincidunt, nec viverra libero pharetra. Sed at feugiat sem. Donec euismod sem non ligula scelerisque vestibulum. Duis eget iaculis tortor, sed viverra lacus. Mauris ultricies sed nulla a eleifend. Cras consectetur porta risus, sit amet vestibulum arcu molestie sit amet. Sed dapibus dolor id lectus semper, vitae euismod leo sodales. Quisque vel vehicula dui. Integer aliquet odio arcu, vitae efficitur odio consequat vitae. Pellentesque augue erat, ornare id laoreet ut, rutrum ac arcu. Maecenas consectetur est risus, et lobortis quam interdum vitae. Quisque lorem lectus, suscipit a est nec, iaculis bibendum neque.
+  </p>
+</div>
 
 <script>
 countdown('timer', <?php echo "'{$results['looptijdeindedag2']} {$results['looptijdtijdstip']}'"; ?>);

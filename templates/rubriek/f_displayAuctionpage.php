05d62fcd03790ee1614340b8e0f096996685aa33
Twan Bolwerk <twanbolwerk@gmail.com>
2018-06-04 14:52:20 +0200

    foutje

--- templates/rubriek/f_displayAuctionpage.php
+++ templates/rubriek/f_displayAuctionpage.php
@@ -57,7 +57,7 @@ ON vw.voorwerpnummer=bd.voorwerpnummer WHERE vw.geblokkeerd = 0");
 
       $countdown = $closingtime;
 
-      $beschrijving = strip_tags($row['beschrijving'],<br>);
+      $beschrijving = strip_tags($row['beschrijving'],'<br>');
 
       $auctionpage.='
 

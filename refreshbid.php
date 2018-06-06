e619111340acae7bbb3a2a9ca74054687a0a88ca
Twan Bolwerk <twanbolwerk@gmail.com>
2018-06-06 14:50:38 +0200

    merge

--- refreshbid.php
+++ refreshbid.php
@@ -2,12 +2,15 @@
 
 require("templates/connect.php");
 
-$statement = $dbh->prepare("select bodbedrag, gebruikersnaam from Bod where voorwerpnummer = ? and bodbedrag = (
-  select max(bodbedrag) from Bod where voorwerpnummer = ?
-  )");
+$statement = $dbh->prepare("select top(1) startprijs, (select max(bodbedrag) from Bod where voorwerpnummer = ?) as maxbod from Voorwerp left outer join Bod on Bod.voorwerpnummer = Voorwerp.voorwerpnummer where Voorwerp.voorwerpnummer = ?");
 $statement->execute(array($_GET['id'], $_GET['id']));
-$maxbid = $statement->fetch(PDO::FETCH_NUM);
+$result = $statement->fetch(PDO::FETCH_NUM);
+$maxbid = "€" . $result[1];
 
-echo "€{$maxbid[0]}";
+if ($maxbid == "€") {
+  $maxbid .= $result[0];
+}
+
+echo $maxbid;
 
 ?>

f865b1df3a7f619bf7de02439d22224e39f3a0d9
JannoOnink <38607600+JannoOnink@users.noreply.github.com>
2018-06-06 14:33:43 +0200

    modify homepage

--- templates/homepage/f_displayAuction.php
+++ templates/homepage/f_displayAuction.php
@@ -3,7 +3,6 @@
 function displayAuction()
 {
 
-
 	global $dbh;
 	global $auction;
 	$auction = "";
@@ -16,66 +15,66 @@ ON vw.voorwerpnummer=b2.voorwerpnummer
 LEFT JOIN (
  SELECT DISTINCT voorwerpnummer,(SELECT TOP 1 bodbedrag FROM Bod b1 where b1.voorwerpnummer=bd.voorwerpnummer
  ORDER BY bodbedrag DESC ) as 'hoogsteBod' from Bod bd ) as bd
- ON vw.voorwerpnummer=bd.voorwerpnummer WHERE vw.geblokkeerd = 0");
-    $i=0;
+ ON vw.voorwerpnummer=bd.voorwerpnummer WHERE vw.geblokkeerd = 0 AND vw.veilinggesloten = 0 ORDER BY looptijdeindedag, looptijdtijdstip ASC");
+    $i = 0;
 		while ($row = $data->fetch()) {
-      $voorwerpnummer = $row[0];
+      $id = $row[0];
       $i++;
-      $timer="timer".$i;
-      $looptijd = $row['looptijd'];
-      $looptijdbegindag =strtotime($row['looptijdbegindag']);
-      $looptijdbegintijdstip = strtotime($row['looptijdtijdstip']);
+      $timer = "timer{$i}";
+			$maxbid = "maxbid{$i}";
 
-             $time = date_create($row['looptijdeindedag'] . $row['looptijdtijdstip']);
-             $closingtime = date_format($time, "d M Y H:i"); //for example 14 Jul 2020 14:35
+			$countdown = "{$row['looptijdeindedag']} {$row['looptijdtijdstip']}";
 
-						 $titel = strip_tags($row['titel']);
-			       $beschrijving = strip_tags($row['beschrijving'],'<br>');
+			$title = strip_tags($row['titel']);
+			$description = strip_tags($row['beschrijving'],'<br>');
 
-             $countdown = $closingtime;
-
-             if(isset($row['hoogsteBod'])){
-             		$huidige_bod = number_format($row['hoogsteBod'], 2, ',', '.');
-             	}else{
-             	  $huidige_bod=$row['startprijs'];
-             	}
-
-			$auction.="  <div class='col-12 col-md-6 col-lg-4'>
+			$auction .= <<<HTML
+				<div class='col-12 col-md-6 col-lg-4'>
           <div class='card auction-card'>
             <div class='view overlay'>
-              <img class='card-img-top' src='".$row['filenaam']."' alt='".$titel."' />
+              <img class='card-img-top' src='{$row['filenaam']}' alt='{$title}' />
             </div>
             <div class='card-body'>
-              <span class='small-font'>".$voorwerpnummer."</span>
-              <h4 class='card-title'>".$titel." #".$voorwerpnummer."</h4>
+              <span class='small-font'>{$id}</span>
+              <h4 class='card-title'>{$title}</h4>
               <hr>
               <div class='card-text'>
-                <p>
-                ".$beschrijving."
-                </p>
-              </div>
-              <hr />
-              <ul class='list-unstyled list-inline d-flex'>
-                <li class='list-inline-item flex-1 ml-5'><i class='fa fa-lg fa-gavel pr-2'></i>&euro;".$huidige_bod."</li>
+              	<p>{$description}</p>
+	            </div>
+	            <hr />
+	            <ul class='list-unstyled list-inline d-flex'>
+	              <li class='list-inline-item flex-1 ml-5'><i class='fa fa-lg fa-gavel pr-2'></i><div style='display:inline;' id='{$maxbid}'></div></li>
 								<div class='card-line'></div>
-                <li class='list-inline-item flex-1 mr-5'><i class=''></i><div id=".$timer."></div></li>
-              </ul>
-            </div>
+	              <li class='list-inline-item flex-1 mr-5'><i class=''></i><div id={$timer}></div></li>
+	            </ul>
+	          </div>
 
             <div class='view overlay mdb-blue'>
-              <a href='detailpage.php?id=".$voorwerpnummer."' class='veiling-bieden'><div class='mask flex-center rgba-white-slight waves-effect waves-light'></div>
+              <a href='detailpage.php?id={$id}' class='veiling-bieden'><div class='mask flex-center rgba-white-slight waves-effect waves-light'></div>
                   <p style='text-align:center'>Bieden</p>
                 </div>
               </a>
             </div>
           </div>
           <script>
-          countdown('".$timer."','".$countdown."');
-          </script>
+          countdown('{$timer}','{$countdown}');
 
-          ";
+					var x = setInterval(function() {
+					  var xhttp;
+					  xhttp = new XMLHttpRequest();
+					  xhttp.onreadystatechange = function() {
+					    if (this.readyState == 4 && this.status == 200) {
+					    document.getElementById('{$maxbid}').innerHTML = this.responseText;
+					    }
+					  };
+					  xhttp.open('GET', 'refreshbid.php?id={$id}', true);
+					  xhttp.send();
+					}, 2000);
+
+          </script>
+HTML;
 		}
-	}catch(PDOException $e){
+	} catch(PDOException $e) {
 		$error = $e;
 	}
 

118143060fe33efb7f8802e792d4183fe9f0cb23
JannoOnink <38607600+JannoOnink@users.noreply.github.com>
2018-06-07 15:47:47 +0200

    fix detailpage

--- refreshbid.php
+++ refreshbid.php
@@ -5,15 +5,15 @@ require("templates/connect.php");
 //Select the starting price and highest bid
 $statement = $dbh->prepare("select top(1) startprijs, (select max(bodbedrag) from Bod where voorwerpnummer = ?) as maxbod from Voorwerp left outer join Bod on Bod.voorwerpnummer = Voorwerp.voorwerpnummer where Voorwerp.voorwerpnummer = ?");
 $statement->execute(array($_GET['id'], $_GET['id']));
-$result = $statement->fetch(PDO::FETCH_NUM);
+$result = $statement->fetch();
 
 //Attempt to get the max bid
-$maxbid = "€" . $result[1];
+$maxbid = "€" . $result['maxbod'];
 
 //If there haven't been any bids then $maxbid will be mostly empty
 if ($maxbid == "€") {
   //Set $maxbid to the starting price instead
-  $maxbid .= $result[0];
+  $maxbid .= $result['startprijs'];
 }
 
 //Return the highest bid (or starting price)
